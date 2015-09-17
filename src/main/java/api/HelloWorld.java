84aaa55cc4e99fcc48adc08e401b5aac0d3fc884
takashima-e <takashima@enrapt.jp>
2015-09-17 13:23:25 +0900

    resolve dependencies issue

--- src/main/java/api/HelloWorld.java
+++ src/main/java/api/HelloWorld.java
@@ -1,5 +1,14 @@
 package api;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 public class HelloWorld {
+
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(){
+        return "Hello World!";
+    }
 }
